c3d946f2a1a32cf8a9156d0460c9580c7ab00424
unknown <ab0989598565@gmail.com>
2025-05-02 17:55:49 +0800

    fix: 替換 st.experimental_rerun 為 st.rerun

--- lab_budget_app/app.py
+++ lab_budget_app/app.py
@@ -14,7 +14,7 @@ if not st.session_state["authenticated"]:
     password = st.text_input("請輸入密碼：", type="password")
     if password == PASSWORD:
         st.session_state["authenticated"] = True
-        st.experimental_rerun()
+        st.rerun()
     else:
         st.stop()
 
